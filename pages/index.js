4c33c419f8c1c36a71f2249028152d6b38e4ed9a
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-06-23 16:45:06 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen text-center p-10">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Pawradise Estero 🐾</h1>
+      <p className="text-lg text-gray-600">Visit our <a href="/unlimited-bathing" className="text-blue-600 underline">Unlimited Bathing</a> page to get started!</p>
+    </main>
+  );
+}
 
